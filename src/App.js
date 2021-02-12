95ef3c450823209985ffb8ff287e83062b6c079e
Mario Reid <mario.reid83@gmail.com>
2021-02-11 19:17:01 -0500

    updated App.js to render employee info

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, {Component} from 'react';
+
+import EmployeeCard from './components/EmployeeCard'
+
+import sampleEmployee from './data/sampleEmployee.json';
+
+class App extends Component {
+  state = {
+    employeeList: [],
+    first: sampleEmployee.results[0].name.first,
+    last: sampleEmployee.results[0].name.last,
+    city: sampleEmployee.results[0].location.city,
+    state: sampleEmployee.results[0].location.state,
+    email: sampleEmployee.results[0].email
+  };
+
+
+
+  render() {
+    return (
+      <div className="App">
+        <EmployeeCard
+          first={this.state.first}
+          last={this.state.last}
+          city={this.state.city}
+          state={this.state.state}
+          email={this.state.email}
+        />
+      </div>
+    )
+  }
+
 }
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
---
+++ src/components/EmployeeCard/index.js
@@ -0,0 +1,62 @@
+import React, { Component } from "react";
+// import "./style.css";
+
+class EmployeeCard extends Component {
+
+state = {
+    first: this.props.first,
+    last: this.props.last,
+    city: this.props.city,
+    state: this.props.state,
+    email: this.props.email
+};
+
+render() {
+    return (
+        <div className="card">
+        <div className="content">
+          <ul>
+            <li>
+              <strong>Name:</strong> {this.state.first} {this.state.last}
+            </li>
+            <li>
+              <strong>Location:</strong> {this.state.city} {this.state.state}
+            </li>
+            <li>
+              <strong>Email:</strong> {this.state.email}
+            </li>
+          </ul>
+        </div>
+      </div>
+    )
+}
+
+}
+
+// function FriendCard(props) {
+//   return (
+//     <div className="card">
+//       <div className="img-container">
+//         <img alt={props.name} src={props.image} />
+//       </div>
+//       <div className="content">
+//         <ul>
+//           <li>
+//             <strong>Name:</strong> {props.name}
+//           </li>
+//           <li>
+//             <strong>Occupation:</strong> {props.occupation}
+//           </li>
+//           <li>
+//             <strong>Location:</strong> {props.location}
+//           </li>
+//         </ul>
+//       </div>
+//       <span onClick={() => props.removeFriend(props.id)} className="remove">
+//         𝘅
+//       </span>
+//     </div>
+//   );
+// }
+
+export default EmployeeCard;
